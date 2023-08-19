bfc9db931e019c92922a9bf078858acb4d0ca872
Max Taldykin <jorpic@gmail.com>
2023-08-19 14:16:50 +0400

    simplify birds sync

--- task/sync_birds.sh
+++ task/sync_birds.sh
@@ -4,16 +4,9 @@
 
 function sync_birds() {
   while read -r event ; do
-    echo sync_birds: $event 1>&2
-    # FIXME: upsert
-    sqlite3 db 2>&1 <<EOF
-      insert into bird(bid, name, role)
-        values (
-          $(jq -r .value.bid <<< "$event"),
-          '$(jq -r .value.name <<< "$event")',
-          '$(jq -r .value.role <<< "$event")'
-        )
-EOF
+    echo -n "$event" \
+      | jq -r '.value | [.bid, .name, .role]| @csv' \
+      | sqlite3 -csv db ".import '|cat -' bird"
   done
 }
 
